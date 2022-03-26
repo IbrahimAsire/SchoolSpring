ce11e46fd1272a274fa3e3e5b6368d5965f11278
ibrahim asiri <ibrahim@MacBook-Pro-ibrahim.local>
2022-03-26 20:46:30 +0300

    feat: add controller of student

---
+++ src/main/java/com/example/school/model/Student.java
@@ -0,0 +1,11 @@
+package com.example.school.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor @Data
+public class Student {
+    private String id;
+    private String name;
+    private String level;
+}
