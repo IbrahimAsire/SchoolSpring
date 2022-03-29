31eac5428090f77842d59326a00fe86217e8a89c
Ibrahim <i-m-6@hotmail.com>
2022-03-29 18:09:51 +0300

    refactoring: model validation

--- src/main/java/com/example/school/model/Student.java
+++ src/main/java/com/example/school/model/Student.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor @Data
 public class Student {
     @NotEmpty(message = "ID is empty!")
+    @Size(min = 2, max = 10)
     private String id;
     @NotEmpty(message = "ID is empty!")
     private String name;
--- src/main/java/com/example/school/model/Teacher.java
+++ src/main/java/com/example/school/model/Teacher.java
@@ -5,10 +5,12 @@ import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor @Data
 public class Teacher {
     @NotEmpty(message = "ID is empty!")
+    @Size(min = 2, max = 9)
     private String id;
     @NotEmpty(message = "Name is empty!")
     private String name;
